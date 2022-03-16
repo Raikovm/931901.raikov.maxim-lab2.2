f56a5b286e4c94fabdf4dde600bd3286531f2730
Максим Райков <raikovms@hotmail.com>
2022-03-16 22:55:17 +0700

    init commit

---
+++ Controllers/CalcServiceController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_Lab_2.Models;
+
+namespace WEB_Lab_2.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+
+        public IActionResult Index() => View();
+
+        public IActionResult ManualParsingSingle()
+        {
+            if (Request.Method != "POST") 
+                return View("Window");
+            
+            try
+            {
+                var calc = new CalcModel
+                {
+                    X = float.Parse(HttpContext.Request.Form["x"]),
+                    Operation = HttpContext.Request.Form["operation"],
+                    Y = float.Parse(HttpContext.Request.Form["y"])
+                };
+
+                ViewBag.Result = calc.Calc();
+            }
+            catch
+            {
+                ViewBag.Result = "Invalid input";
+            }
+
+            return View("Result");
+        }
+
+
+
+        [HttpGet]
+        [ActionName("ManualParsingSeparate")]
+        public IActionResult ManualParsingSeparateGet() => View("Window");
+
+        [HttpPost]
+        [ActionName("ManualParsingSeparate")]
+        public IActionResult ManualParsingSeparatePost()
+        {
+            try
+            {
+                var calc = new CalcModel
+                {
+                    X = float.Parse(HttpContext.Request.Form["x"]),
+                    Operation = HttpContext.Request.Form["operation"],
+                    Y = float.Parse(HttpContext.Request.Form["y"])
+                };
+
+                ViewBag.Result = calc.Calc();
+            }
+            catch
+            {
+                ViewBag.Result = "Invalid input";
+            }
+
+            return View("Result");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ModelBindingParameters() => View("Window");
+
+        [HttpPost]
+        public IActionResult ModelBindingParameters(int x, string operation, int y)
+        {
+            if (ModelState.IsValid)
+            {
+                var calc = new CalcModel
+                {
+                    X = x,
+                    Operation = operation,
+                    Y = y
+                };
+                ViewBag.Result = calc.Calc();
+            }
+            else
+            {
+                ViewBag.Result = "Invalid input";
+            }
+
+            return View("Result");
+        }
+
+        [HttpGet]
+        public IActionResult ModelBindingSeparate() => View("Window");
+
+        [HttpPost]
+        public IActionResult ModelBindingSeparate(CalcModel model)
+        {
+            ViewBag.Result = ModelState.IsValid
+                ? model.Calc()
+                : "Invalid input";
+
+            return View("Result");
+        }
+    }
+}
---
+++ Models/CalcModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WEB_Lab_2.Models
+{
+    public class CalcModel
+    {
+        public float X { get; init; }
+        public float Y { get; init; }
+        public string? Operation { get; init; }
+
+        public string Calc() =>
+            Operation switch
+            {
+                "+" => $"{X} + {Y} = {X + Y}",
+                "-" => $"{X} - {Y} = {X - Y}",
+                "*" => $"{X} * {Y} = {X * Y}",
+                "/" => $"{X} / {Y} = {X / Y}",
+                _ => "Invalid operation"
+            };
+    }
+}
